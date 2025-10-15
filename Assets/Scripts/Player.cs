a1984846b8d53fa0feaefd0aee6193a6cde47200
sz5804 <sz5804@rit.edu>
2025-10-15 14:47:08 -0400

    World Scene Setup WIP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,10 +2,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            // If another instance already exists, destroy this one.
+            Destroy(gameObject);
+        }
+        else
+        {
+            // Otherwise, set this as the instance.
+            Instance = this;
+            // Prevents from being destroyed on scene load
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
     // Update is called once per frame
